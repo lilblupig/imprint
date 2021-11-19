14a9302144899038a51e5285f1dce994811ff4b4
lilblupig <lilblupig@gmail.com>
2021-11-19 20:15:31 +0000

    Refactor code as getting clogged up

--- app.py
+++ app.py
@@ -1,72 +1,29 @@
-""" Import dependencies """
+# Import dependencies
 import os
-from flask import (
-    Flask,
-    flash,
-    render_template,
-    redirect,
-    request,
-    session,
-    url_for
-)
+from flask import Flask
 from flask_pymongo import PyMongo
-from bson.objectid import ObjectId
-from werkzeug.security import generate_password_hash, check_password_hash
-from flask_mail import Message, Mail
-if os.path.exists("env.py"):
-    import env
-from forms import ContactForm
 
-mail = Mail()
+# Get environment variables
+if os.path.exists("config/env.py"):
+    from config import env
 
+# Initiaite Flask application
 app = Flask(__name__)
 
+# Define database access variables
 app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 app.secret_key = os.environ.get("SECRET_KEY")
 
+# Define ReCaptcha secret keys
 app.config["RECAPTCHA_PUBLIC_KEY"] = os.environ.get("C_SITE_KEY")
 app.config["RECAPTCHA_PRIVATE_KEY"] = os.environ.get("C_SECRET_KEY")
 
-app.config["MAIL_SERVER"] = os.environ.get("MAIL_SERVER")
-app.config["MAIL_PORT"] = os.environ.get("MAIL_PORT")
-app.config["MAIL_USE_SSL"] = os.environ.get("MAIL_USE_SSL")
-app.config["MAIL_USERNAME"] = os.environ.get("MAIL_USERNAME")
-app.config["MAIL_PASSWORD"] = os.environ.get("MAIL_PASSWORD")
+# Get routes
+from routes.routes import *
 
-mail.init_app(app)
-
+# Set PyMongo variable
 mongo = PyMongo(app)
-
-
-@app.route("/")
-@app.route("/get_locations")
-def get_locations():
-    """ Define test function """
-    locations = mongo.db.locations.find()
-    return render_template("index.html", locations=locations)
-
-# Mail sending works with email hard coded into recipient - not working with environment variable - to address later
-@app.route("/contact", methods=["GET", "POST"])
-def contact():
-    """ Contact form """
-    form = ContactForm()
-
-    if request.method == 'POST':
-        if form.validate() == False:
-            return render_template('contact.html', form=form)
-        else:
-            msg = Message("Imprint contact message", sender="MAIL_USERNAME", recipients=["MAIL_USERNAME"])
-            msg.body = """
-            From: %s <%s>
-            %s
-            """ % (form.name.data, form.email.data, form.body.data)
-            mail.send(msg)
-
-            return 'Form posted.'
-    elif request.method == 'GET':
-        return render_template("contact.html", form=form)
-
 
 if __name__ == "__main__":
     app.run(
---
+++ routes/routes.py
@@ -0,0 +1,57 @@
+from config import mail
+
+import os
+from app import app, mongo
+from flask import (
+    flash,
+    render_template,
+    redirect,
+    request,
+    session,
+    url_for
+)
+
+from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_mail import Message, Mail
+
+from forms import *
+
+mail = Mail()
+
+app.config["MAIL_SERVER"] = os.environ.get("MAIL_SERVER")
+app.config["MAIL_PORT"] = os.environ.get("MAIL_PORT")
+app.config["MAIL_USE_SSL"] = os.environ.get("MAIL_USE_SSL")
+app.config["MAIL_USERNAME"] = os.environ.get("MAIL_USERNAME")
+app.config["MAIL_PASSWORD"] = os.environ.get("MAIL_PASSWORD")
+
+mail.init_app(app)
+
+
+@app.route("/")
+@app.route("/get_locations")
+def get_locations():
+    """ Define test function """
+    locations = mongo.db.locations.find()
+    return render_template("index.html", locations=locations)
+
+# Mail sending works with email hard coded into recipient - not working with environment variable - to address later
+@app.route("/contact", methods=["GET", "POST"])
+def contact():
+    """ Contact form """
+    form = ContactForm()
+
+    if request.method == 'POST':
+        if form.validate() == False:
+            return render_template('contact.html', form=form)
+        else:
+            msg = Message("Imprint contact message", sender="MAIL_USERNAME", recipients=["MAIL_USERNAME"])
+            msg.body = """
+            From: %s <%s>
+            %s
+            """ % (form.name.data, form.email.data, form.body.data)
+            mail.send(msg)
+
+            return 'Form posted.'
+    elif request.method == 'GET':
+        return render_template("contact.html", form=form)
