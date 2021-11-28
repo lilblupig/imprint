fd36a24c56942e03e15b1919522d9166375244df
lilblupig <lilblupig@gmail.com>
2021-11-28 16:56:35 +0000

    Comments for Cloudinary code

--- config/cloudinary_config.py
+++ config/cloudinary_config.py
@@ -2,13 +2,14 @@
     Imprint Nov 2021
     Cloudinary configuration for image management
 """
-
+# Import dependencies
 import os
 import cloudinary
 import cloudinary.uploader
 import cloudinary.api
 
-cloudinary.config( 
+# Get Cloudinary account data
+cloudinary.config(
   cloud_name=os.environ.get("CLOUD_NAME"),
   api_key=os.environ.get("API_KEY"),
   api_secret=os.environ.get("API_SECRET")
