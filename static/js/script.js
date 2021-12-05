fbc091d69aac6c412c7f488b451d30e8338e4a37
lilblupig <lilblupig@gmail.com>
2021-12-05 14:34:46 +0000

    Replace Materialize for Bootstrap re accessibility

--- static/js/script.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function(){
-    /* Materialize - Display mobile navigation on right side of screen */
-    $('.sidenav').sidenav({edge: "right"});
-    /* Materialize - Display form Select field */
-    $('select').formSelect();
-});
