a5f688cd4f4a6e4d14bb4117a8971038addf926f
lilblupig <lilblupig@gmail.com>
2021-12-12 16:14:51 +0000

    Form submit on enter where no recaptcha

--- static/js/script.js
+++ static/js/script.js
@@ -3,4 +3,13 @@ $( document ).ready(function() {
     /* Have form focus set to first field on input pages */
     $( "#username, #name" ).focus();
 
+    /* Allow form submit on enter where no recaptcha */
+    $( ".submit-listen" ).keydown(function(event) {
+        if (event.key === "Enter") {
+            console.log("Enter was pressed")
+            $( "#submit" ).click();
+            return false;
+        }
+    })
+
   });
