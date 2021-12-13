155eb7c02fbb2911ff6da4ef2fdda5528854e9a0
lilblupig <lilblupig@gmail.com>
2021-12-13 18:51:41 +0000

    Generic error handler

--- routes/errors.py
+++ routes/errors.py
@@ -26,3 +26,12 @@ def error_internal_server(error):
     message = "Sorry, we seem to be experiencing some difficulty."
     user_help = "Choose from the buttons below, or the main menu to get back on track."
     return render_template('errors.html', message=message, help=help), 500
+
+
+@app.errorhandler(Exception)
+def error_generic(error):
+    """ Error handler for all other exceptions """
+    message = "Sorry, something went wrong."
+    user_help = "Choose from the buttons below, or the main menu to get back on track."
+    return render_template('errors.html', message=message, help=help), 500
+
