fe7e0b038addbeb5db5d7dcc2719bb9fa8d81c9d
lilblupig <lilblupig@gmail.com>
2021-12-13 19:09:21 +0000

    Remove use of reserved word help

--- routes/errors.py
+++ routes/errors.py
@@ -17,7 +17,7 @@ def error_page_not_found(error):
     """ Error handler for 404 """
     message = "Sorry, we can't find that page"
     user_help = "Choose from the buttons below, or the main menu to get back on track."
-    return render_template('errors.html', message=message, help=help), 404
+    return render_template('errors.html', message=message, user_help=user_help), 404
 
 
 @app.errorhandler(500)
@@ -25,7 +25,7 @@ def error_internal_server(error):
     """ Error handler for 500 """
     message = "Sorry, we seem to be experiencing some difficulty."
     user_help = "Choose from the buttons below, or the main menu to get back on track."
-    return render_template('errors.html', message=message, help=help), 500
+    return render_template('errors.html', message=message, user_help=user_help), 500
 
 
 @app.errorhandler(Exception)
@@ -33,4 +33,4 @@ def error_generic(error):
     """ Error handler for all other exceptions """
     message = "Sorry, something went wrong."
     user_help = "Choose from the buttons below, or the main menu to get back on track."
-    return render_template('errors.html', message=message, help=help), 500
+    return render_template('errors.html', message=message, user_help=user_help), 500
