82ee8a4db69bed6ef32822c4c179fc0e7611a56b
lilblupig <lilblupig@gmail.com>
2021-12-14 18:54:21 +0000

    Email specific exception handler per RF

--- routes/routes.py
+++ routes/routes.py
@@ -137,11 +137,16 @@ def contact():
             }
 
             # Call mail function and provide form data
-            mail_config.send_email(form_content)
+            try:
+                mail_config.send_email(form_content)
+                # If unable to send, flash failure message
+            except Exception as e:
+                flash("unable to send email, please try again later")
+                # Otherwise, flash success message
+            else:
+                flash("Thank you for your message, we will be in touch as soon as we can.")
 
-            flash("Thank you for your message, we will be in touch as soon as we can.")
-
-            # Display success message for user
+            # Redirect user to gallery page
             return redirect(url_for("gallery"))
 
     return render_template("contact.html", form=form)
