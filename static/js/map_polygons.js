4cb17e632da5c7e2b090410d23306cb6c4e489c8
lilblupig <lilblupig@gmail.com>
2021-12-18 17:25:17 +0000

    Create test polygon

---
+++ static/js/map_polygons.js
@@ -0,0 +1,19 @@
+// Provide Maps function with placeholder polygon coordinates to eliminate console error
+const placeholderMap = [
+    { lng: -1.87818062, lat: 50.71513748 },
+    { lng: -1.87818062, lat: 50.71513748 },
+    { lng: -1.87818062, lat: 50.71513748 },
+];
+
+// Coordinates for outline of Bournemouth from GADM database: https://gadm.org/download_country_v3.html
+const angelSquare = [
+    { lat: 51.00717365160647, lng: -2.194208271493193 },
+    { lat: 51.00675565535095, lng: -2.193782926823142 },
+    { lat: 51.00661131044534, lng: -2.193462723532204 },
+    { lat: 51.006352691354785, lng: -2.19354396913789 },
+    { lat: 51.00593769493529, lng: -2.193773368510502 },
+    { lat: 51.00599483234391, lng: -2.194566708007452 },
+    { lat: 51.00636472018112, lng: -2.1948964696055815 },
+    { lat: 51.00667145419929, lng: -2.194633616157797 },
+    { lat: 51.00717365160647, lng: -2.194208271493193 },
+];
--- static/js/maps.js
+++ static/js/maps.js
@@ -2,11 +2,30 @@
 Google Maps API - displays a map of the regions
 */
 
+const mapCentres = {
+    "": { lng: -2.195737342140075, lat: 51.00656544855768 },
+    "angelSquare": { lng: -2.1941698786790353, lat: 51.006356916697335 },
+    "rTwo": { lng: -2.60000, lat: 51.45000 },
+};
+
 function initMap() {
     // Produces the map, determines initial zoom level and map center
     const map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 10,
-        center: { lat: -34.397, lng: 150.644 },
+        zoom: 17,
+        center:  mapCentres["angelSquare"],
       });
+
+    // Renders a polygon of each region on the map
+    // Polygon co-ordinates stored in static/js/map_polygons.js
+    const mapPolygon = new google.maps.Polygon({
+        paths: angelSquare,
+        strokeColor: "#fbb13c",
+        strokeOpacity: 0.5,
+        strokeWeight: 1,
+        fillColor: "#fbb13c",
+        fillOpacity: 0.35,
+    });
+
+    mapPolygon.setMap(map);
 
 }
